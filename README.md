6165781261d524afb754e4010a70cbc50253d931
tanyay532 <tanyay532@gmail.com>
2021-03-04 20:36:58 +0530

    Short film app setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Short Film App
---
+++ tests/test.test.js
@@ -0,0 +1,10 @@
+import { Selector, ClientFunction } from 'testcafe';
+
+fixture`Getting started`.page`https://github.com/`
+
+
+test('Sign in', async t => {
+    const signInLink = Selector('.HeaderMenu-link').withText('Sign in')
+    await t
+        .click(signInLink)
+});
