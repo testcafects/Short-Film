c854b0922b4ffaf18bfb8b9f916cd4972ef03285
rakeshramesh98 <rakesh98raji@gmail.com>
2021-03-10 09:58:26 +0530

    feat: 🎸 add card and cardlist styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './App.css';
 import CheckoutBox from './molecules/CheckoutBox/CheckoutBox.js'
 import Headers from './molecules/header/header.js'
+import Cards from  './molecules/Cards/Cards.js'
 function App() {
   return (
     <div className="App">
     <Headers />
       <CheckoutBox />
+      <div className="card-list">
+                      
+                      {
+                        [1, 2, 3, 4,5].map((det) => <Cards key={det}/>)
+                       }
+                      
+                        
+     </div>
       
     </div>
   );
--- src/atoms/Buttons/Buttons.js
+++ src/atoms/Buttons/Buttons.js
@@ -6,7 +6,7 @@ import { Button } from 'antd';
 const Buttons = props => {
 return (
 <div className={style['a-buttons']}>
-    <Button className={style['m-checkout-box__btn']} id={props.id} type="primary">{props.name}</Button>
+    <Button className={style['m-checkout-box__btn']} id={props.id} type="primary" style={{width:props.width, height:props.height}}>{props.name}</Button>
 </div>
 );
 };
